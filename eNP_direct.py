04784dd3e766e2a47c56c040f3399243456978fb
epence <ericpence19@gmail.com>
2022-04-14 13:27:33 -0400

    Added search for eps that satisfies delta

--- eNP_direct.py
+++ eNP_direct.py
@@ -2,6 +2,7 @@ from scipy.stats import binom
 from math import exp
 from decimal import *
 import numpy as np
+import scipy
 
 
 def noiseless_privacy_analysis(n, p, eps):
@@ -106,23 +107,71 @@ def noiseless_privacy_analysis(n, p, eps):
     # print("good a range: [", lower_a, ",", upper_a, ")")
     return private_range, expected_val_of_indicator
 
+def compute_epsilon_given_delta(delta, p, n, eps_search_space = [0.0, 7.0]):
+    '''
+    Computes smallest epsilon for a given delats (failure rate) by identifying 
+    the epsilon value that minimizes the difference between the specfied delta and 
+    the actual delta (a function of epsilon)
 
-n_vals = [10, 100]
-p_vals = [0.1, 0.5, 0.75]
+    The significance here is that a practioner might ask, "how do I set epsilon
+    to ensure eNP holds x% of the time?"
+
+    Args:
+    delta <float> : acceptable failure rate of eNP
+    p <float> : probability of t_i=1 (for any t_i in input T)
+    n <int> : number of inputs, or participants
+    eps_search_space <list> : list of 2 floats specifcying the [lower, upper] bounds on potential epsilon values
+    
+    Returns:
+    eps_out <float> : epsilon required to achieve specfied delta
+    '''
+    # Observation: small changes in epsilon do not seem to change failure likelihood
+    def f(eps, delta, p, n):
+        private_range, exp_val = noiseless_privacy_analysis(n, p, eps)
+        computed_delta = 1-exp_val
+        # if computed delta is much smaller than delta, then this is very negative which is better
+        return computed_delta - delta
+    
+    eps_opt = scipy.optimize.fminbound(f, eps_search_space[0], eps_search_space[1], args=(delta, p, n,))
+    return eps_opt
+
+def compute_epsilon_given_delta_full_range(delta, p, n, eps_search_space = [0.01, 10.0, 0.01]):
+    for eps in np.arange(eps_search_space[0], eps_search_space[1], eps_search_space[2]):
+        private_range, exp_val = noiseless_privacy_analysis(n, p, eps)
+        if exp_val > 1:
+            # expected value is greater than 1 sometimes
+            # print("expected value greater than 1, setting to 1")
+            # print(exp_val)
+            exp_val = 1
+        if private_range == (1,n-1):
+            computed_delta = 1-exp_val
+            if computed_delta <= delta:
+                print(computed_delta, eps)
+                return eps
+    raise ValueError("not satisfiable")
+
+
+n_vals = range(80,81) # smallest n with reasonable delts is 30 or 31
+p_vals = np.arange(0.1, 1.0, 0.1)
 # According to NIST (https://www.nist.gov/blogs/cybersecurity-insights/differential-privacy-future-work-open-challenges)
 # Epsilon values between 0 and 5 are strong
 eps_vals = np.arange(0.1, 5.1, 0.1)
 
-for n in n_vals:
-    for p in p_vals:
-        for eps in eps_vals:
-            # Delta represents the probability that eNP does not hold
-            # Delta = 1-E[I]
-            # According to (https://www.ftc.gov/system/files/documents/public_comments/2017/11/00023-141742.pdf)
-            # Delta should be 1/(1 billion) = 1e-9
-            private_range, exp_val = noiseless_privacy_analysis(n, p, eps)
-            delta = 1 - exp_val
-            print("good a range: [", private_range[0], ",", private_range[1], ")")
-            print("(n,p,eps,delta) =",(n, p, eps, delta), "\n")
-            if delta <= 1e-9:
-                print("Good Delta ^^\n")
+compute_epsilon_given_delta_full_range(delta = 1e-9, p=0.5, n=50)
+print(compute_epsilon_given_delta(delta = 1e-9, p=0.5, n=50))
+print(noiseless_privacy_analysis(n=50,p=0.5,eps=3.8))
+
+
+# for n in n_vals:
+#     for p in p_vals:
+#         for eps in eps_vals:
+#             # Delta represents the probability that eNP does not hold
+#             # Delta = 1-E[I]
+#             # According to (https://www.ftc.gov/system/files/documents/public_comments/2017/11/00023-141742.pdf)
+#             # Delta should be 1/(1 billion) = 1e-9
+#             private_range, exp_val = noiseless_privacy_analysis(n, p, eps)
+#             delta = 1 - exp_val
+#             if delta <= 1e-9:
+#                 print("good a range: [", private_range[0], ",", private_range[1], ")")
+#                 print("(n,p,eps,delta) =",(n, p, eps, delta), "\n")
+
