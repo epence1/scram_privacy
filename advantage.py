910e5336d54dff0d73be783266b1bfc6b5a3962a
Leo de Castro <ldec@mit.edu>
2022-05-25 23:58:54 -0400

    noiseless advantage analysis

--- advantage.py
+++ advantage.py
@@ -1,11 +1,17 @@
+from numpy import exp
 from scipy.stats import binom
 from noise import LaplaceNoise
 from decimal import *
 DELTA = 10e-9
 N = 100
-P = 0.3
-MIN_EPS = 0.1
-MAX_EPS = 10
+# N = 30
+# P = 0.3
+# MIN_EPS = 0.1
+# MAX_EPS = 3
+
+MIN_P = 0.25
+MAX_P = 0.75
+P_STEP = 0.05
 
 def compute_prob_input_given_count(count, n):
     '''
@@ -26,46 +32,36 @@ def compute_prob_input_given_count(count, n):
     return prob_0_given_count, prob_1_given_count
 
 # Epsilon to be modulated
-eps = MIN_EPS
-while eps <= MAX_EPS:
-    eps = float(eps)
-    print("\n\n============")
-    print("Epsilon Value: ", eps)
-    # Define Differential Privacy Class
-    dp_noise = LaplaceNoise(eps)
-    # Begin iterating through c in [0,n]
-    prob_correct_list = []
+p = MIN_P
+while p <= MAX_P:
+    expected_correct = 0
     for c in range(0,N):
-        print("Count: ", c)
-        # Calculate noisy count c as per DP requirements (Lap(1/eps))
-        noise = dp_noise.get_sample()
-        noisy_c = round(c + noise)
-        print("Noisy Count: ", noisy_c)
+        # print("Count: ", c)
 
         # Calculate Pr[C=c | t = 1], Pr[C=c | t = 0]
-        prob_c_given_0 = binom.pmf(k=c, n= N - 1, p=P) # fixes nth input as 0
-        prob_c_given_1 = binom.pmf(k=c - 1, n= N - 1, p=P)  # fixes nth input as 1
+        prob_c_given_0 = binom.pmf(k=c, n= N - 1, p=p) # fixes nth input as 0
+        prob_c_given_1 = binom.pmf(k=c - 1, n= N - 1, p=p)  # fixes nth input as 1
 
         #print("(prob_c_given_0, prob_c_given_1): ", prob_c_given_0, prob_c_given_1)
 
-        max_prob = max(prob_c_given_0, prob_c_given_1)
-
         # b (MLE) = max of these two probabilities 
-        if max_prob == prob_c_given_0:
+        if prob_c_given_1 < prob_c_given_0:
             b = 0
         else:
             b = 1
-            
-        print("Bit Guessed: ", b)
+
+        success_rate = compute_prob_input_given_count(c, N)[b]
+        # print("Bit Guessed: ", b)
         # Calculate Pr[t=b | C=c] -- probability that adversary is correct
-        prob_correct_list.append(compute_prob_input_given_count(c, N)[b])
+        # prob_correct_list.append(compute_prob_input_given_count(c, N)[b])
+        expected_correct += binom.pmf(k=c, n=N, p=p) * success_rate
         #print("Prob Guess is Correct: ", compute_prob_input_given_count(c, N)[b])
 
 
     #after finish iterating through c take expectation of adversary's correctness
-    expected_correct = sum(prob_correct_list)/len(prob_correct_list)
-    print("(Epsilon, Expected Correctness): ", eps, expected_correct)
-    eps += 0.1
+    # expected_correct = sum(prob_correct_list)/len(prob_correct_list)
+    print("(p, Expected Correctness, advantage, max_success_rate): ", p, expected_correct, expected_correct - max(p, 1-p))
+    p += P_STEP
 
             
 
